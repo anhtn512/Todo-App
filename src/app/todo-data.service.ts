223ff207df8aec1886db02425dd59a0ac3e8f7da
= <=>
2018-10-18 15:51:34 +0700

    store data to localstorage

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -8,28 +8,36 @@ export class TodoDataService {
 
   lastId: number;
 
-  todos: Todo[] = [];
+  todos: Todo[] = JSON.parse(localStorage.getItem('todos')) || [];
+
+  updateToLocalStorage(): void {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
 
   addTodo(todo: Todo): void {
     if (!todo.id) {
       todo.id = ++this.lastId;
     }
     this.todos.push(todo);
+    this.updateToLocalStorage();
   }
 
   deleteTodo(id: number): void {
     this.todos = this.todos.filter(todo => todo.id !== id);
+    this.updateToLocalStorage();
   }
 
   getTodoById(id: number): Todo {
     return this.todos.filter(todo => todo.id === id).pop();
   }
+
   updateTodoById(id: number, value: Object = { }): Todo {
     let todo = this.getTodoById(id);
     if (!todo) {
       return null;
     }
     Object.assign(todo, value);
+    this.updateToLocalStorage();
   }
 
   getAllTodos(): Todo[] {
@@ -40,6 +48,7 @@ export class TodoDataService {
     let updateTodo = this.updateTodoById(todo.id, {
       complete: !todo.complete
     });
+    this.updateToLocalStorage();
     return updateTodo;
   }
   constructor() {
