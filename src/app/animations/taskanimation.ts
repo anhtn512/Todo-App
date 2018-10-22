c6789be6dfddfbf8bdfc9c05c5398c9239ecfce5
anhtn512 <anhtn512@gmail.com>
2018-10-22 09:51:51 +0700

    add animation for todoapp

--- src/app/animations/taskanimation.ts
+++ src/app/animations/taskanimation.ts
@@ -1,6 +1,18 @@
 import { animate, state, style, transition, trigger, AUTO_STYLE } from '@angular/animations';
 
 export const ON_OFF_TASK_TRANSITION = [
+    trigger('onOffTodoApp', [
+        transition(':enter', [
+            style({ opacity: 0, transform: 'translateY(300px)' }),
+            animate('0.4s ease'),
+            style({ opacity: 1, transform: 'translateY(0px)' })
+        ]),
+        transition(':leave', [
+            style({ opacity: 1, transform: 'translateY(0px)' }),
+            animate('0.4s ease'),
+            style({ opacity: 0, transform: 'translateY(300px)' })
+        ])
+    ]),
     trigger('onOffTaskTransition', [
         transition(':enter', [
             style({ paddingTop: 0, paddingBottom: 0, height: 0, opacity: 0 }),
