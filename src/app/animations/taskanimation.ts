a75c521a374b5de1ec96d8760750bb0bdaa366f2
anhtn512 <anhtn512@gmail.com>
2018-10-24 21:21:53 +0700

    fix css login

--- src/app/animations/taskanimation.ts
+++ src/app/animations/taskanimation.ts
@@ -24,6 +24,34 @@ export const ON_OFF_TASK_TRANSITION = [
             animate('0.2s ease'),
             style({ paddingTop: 0, paddingBottom: 0, height: 0, opacity: 0 })
         ])
+    ]),
+    trigger('moveIn', [
+        // state('void', style({ position: 'fixed', width: '100%' })),
+        // state('*', style({ position: 'fixed', width: '100%' })),
+        transition(':enter', [
+            style({ opacity: '0', transform: 'translateX(100px)' }),
+            animate('.6s ease-in-out', style({ opacity: '1', transform: 'translateX(0)' }))
+        ]),
+        transition(':leave', [
+            style({ opacity: '1', transform: 'translateX(0)' }),
+            animate('.3s ease-in-out', style({ opacity: '0', transform: 'translateX(-200px)' }))
+        ])
+    ]),
+    trigger('fallIn', [
+        transition(':enter', [
+            style({ opacity: '0', transform: 'translateY(40px)' }),
+            animate('.4s .2s ease-in-out', style({ opacity: '1', transform: 'translateY(0)' }))
+        ]),
+        transition(':leave', [
+            style({ opacity: '1', transform: 'translateX(0)' }),
+            animate('.3s ease-in-out', style({ opacity: '0', transform: 'translateX(-200px)' }))
+        ])
+    ]),
+    trigger('moveInLeft', [
+        transition(':enter', [
+            style({ opacity: '0', transform: 'translateX(-100px)' }),
+            animate('.6s .2s ease-in-out', style({ opacity: '1', transform: 'translateX(0)' }))
+        ])
     ])
 
 ];
